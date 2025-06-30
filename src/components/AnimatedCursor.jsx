5e744e43bda628857a7174bc0ff29b9a3a86a2e9
CollinsNugi <emelumbacollins@gmail.com>
2025-06-30 08:27:29 +0100

    cursor

--- src/components/AnimatedCursor.jsx
+++ src/components/AnimatedCursor.jsx
@@ -7,15 +7,26 @@ export default function AnimationCursor() {
     cursor.classList.add('cursor-follower');
     document.body.appendChild(cursor);
 
-    const moveCursor = (e) => {
-      cursor.style.top = `${e.clientY}px`;
-      cursor.style.left = `${e.clientX}px`;
+    const moveCursor = (x, y) => {
+      cursor.style.top = `${y}px`;
+      cursor.style.left = `${x}px`;
     };
 
-    window.addEventListener('mousemove', moveCursor);
+    const handleMouseMove = (e) => moveCursor(e.clientX, e.clientY);
+
+    const handleTouchMove = (e) => {
+      if (e.touches.length > 0) {
+        const touch = e.touches[0];
+        moveCursor(touch.clientX, touch.clientY);
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('touchmove', handleTouchMove);
 
     return () => {
-      window.removeEventListener('mousemove', moveCursor);
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('touchmove', handleTouchMove);
       cursor.remove();
     };
   }, []);
